9c420099056ea5b1734ee5c1075f9f8ea478d1e5
Julian <julian_delisio@hotmail.com>
2022-04-19 22:42:37 -0300

    input y boton

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import TareaFormulario from "./componentes/TareaFormulario";
 import Titulo from "./componentes/Titulo";
-import "./App.css";
+import Tarea from "./componentes/Tarea";
 import logo from "./imagenes/logo.png";
+import "./App.css";
+import "./hojas-de-estilo/Tarea.css";
+import React from 'react'
+
 
 function App() {
   return (
-    <div className="aplicacion-tareas">
-      < Titulo 
-        titulo="To-Do List "
-        logo={logo}
-        alt="logo"
-      />
+    <div>
+      <Titulo titulo="To-Do List " logo={logo} alt="logo" />
+      <div className="aplicacion-tareas">
+        <div className="tareas-lista-principal">
+          <h1 className="titulo-contenedor-tarea">Mis tareas</h1>
+          <TareaFormulario />
+          <Tarea texto="Aprender React" />
+        </div>
+      </div>
     </div>
-    
   );
 }
 
---
+++ src/componentes/TareaFormulario.js
@@ -0,0 +1,21 @@
+import  React from 'react';
+import "../hojas-de-estilo/TareaFormulario.css";
+
+
+function TareaFormulario(props){
+    return (
+        <form className="Tarea-formulario">
+            <input
+            className="tarea-input"
+            type="text"
+            placeholder='Escribe una tarea'
+            name="texto"
+            />
+            <button className="tarea-boton">Agregar Tarea</button>
+        </form>
+    )
+};
+
+export default TareaFormulario;
+
+
