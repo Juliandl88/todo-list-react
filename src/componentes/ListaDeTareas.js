91fcbfd6c8aaa8a60342c2aec408393d91d392fc
Julian <julian_delisio@hotmail.com>
2022-04-25 23:39:17 -0300

    renderizar tareas

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -6,16 +6,24 @@ import "../hojas-de-estilo/ListaDeTareas.css";
 function ListaDeTareas() {
   const [tareas, setTareas] = useState([]);
 
-  const aregarTarea = (tarea) => {
-    console.log("tarea agregada", tarea);
+  const agregarTarea = (tarea) => {
+    if(tarea.texto.trim()){
+        tarea.texto = tarea.texto.trim();
+        const tareasActualizadas = [tarea, ...tareas];
+        setTareas(tareasActualizadas);
+    }
   };
 
   return (
     <>
-      <TareaFormulario />
+      <TareaFormulario onSubmit={agregarTarea}/>
       <div className="tareas-lista-contenedor">
         {tareas.map((tarea) => (
-          <Tarea texto={tarea.texto} />
+          <Tarea 
+          key = {tarea.id}
+          id = {tarea.id}
+          texto = {tarea.texto} 
+          completada = {tarea.completada}/>
         ))}
       </div>
     </>
--- src/componentes/TareaFormulario.js
+++ src/componentes/TareaFormulario.js
@@ -1,5 +1,6 @@
 import  React, {useState} from 'react';
 import "../hojas-de-estilo/TareaFormulario.css";
+import { v4 as uuidv4 } from 'uuid';
 
 
 function TareaFormulario(props){
@@ -12,14 +13,21 @@ function TareaFormulario(props){
     }
 
     const manejarEnvio = e => {
+        e.preventDefault();
+        console.log("Enviando formulario")
+
         const tareaNueva = {
-            id: "34545",
-            texto: "hola"
+            id: uuidv4(),
+            texto: input,
+            completada: false
         }
+       props.onSubmit(tareaNueva);
     }
 
     return (
-        <form className="Tarea-formulario">
+        <form 
+        className="Tarea-formulario"
+        onSubmit={manejarEnvio}>
             <input
             className="tarea-input"
             type="text"
