4be372f9f089ddc7b99d7f6d857e7502d5c7f0ce
Julian <julian_delisio@hotmail.com>
2022-04-28 11:24:54 -0300

    eliminar y modificar tareas

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -19,6 +19,16 @@ function ListaDeTareas() {
     setTareas(tareasActualizadas);
   }
 
+  const completarTarea = id => {
+    const tareasActualizadas = tareas.map(tarea => {
+      if(tarea.id === id){
+        tarea.completada = !tarea.completada;
+      }
+      return tarea;
+    });
+    setTareas(tareasActualizadas);
+  }
+
   return (
     <>
       
@@ -31,6 +41,7 @@ function ListaDeTareas() {
             texto={tarea.texto}
             completada={tarea.completada}
             eliminarTarea = {eliminarTarea}
+            completarTarea = {completarTarea}
           />
         ))}
       </div>
