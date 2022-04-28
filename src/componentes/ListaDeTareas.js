b8046c14670674048730bae9ad5fd5ebffb00903
Julian <julian_delisio@hotmail.com>
2022-04-28 11:17:31 -0300

    eliminar y modificar tareas

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -7,23 +7,31 @@ function ListaDeTareas() {
   const [tareas, setTareas] = useState([]);
 
   const agregarTarea = (tarea) => {
-    if(tarea.texto.trim()){
-        tarea.texto = tarea.texto.trim();
-        const tareasActualizadas = [tarea, ...tareas];
-        setTareas(tareasActualizadas);
+    if (tarea.texto.trim()) {
+      tarea.texto = tarea.texto.trim();
+      const tareasActualizadas = [tarea, ...tareas];
+      setTareas(tareasActualizadas);
     }
   };
 
+  const eliminarTarea = id => {
+    const tareasActualizadas = tareas.filter(tarea => tarea.id !==id);
+    setTareas(tareasActualizadas);
+  }
+
   return (
     <>
-      <TareaFormulario onSubmit={agregarTarea}/>
+      
       <div className="tareas-lista-contenedor">
+      <TareaFormulario onSubmit={agregarTarea} />
         {tareas.map((tarea) => (
-          <Tarea 
-          key = {tarea.id}
-          id = {tarea.id}
-          texto = {tarea.texto} 
-          completada = {tarea.completada}/>
+          <Tarea
+            key={tarea.id}
+            id={tarea.id}
+            texto={tarea.texto}
+            completada={tarea.completada}
+            eliminarTarea = {eliminarTarea}
+          />
         ))}
       </div>
     </>
